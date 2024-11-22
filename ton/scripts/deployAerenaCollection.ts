f3356a2b587d70359a165de73e3f26dd9875f7c3
Ramon Arca <ramonteodoroarca@gmail.com>
2024-11-22 21:47:23 +0800

    Update deployAerenaCollection.ts

--- ton/scripts/deployAerenaCollection.ts
+++ ton/scripts/deployAerenaCollection.ts
@@ -1,6 +1,7 @@
 import { Address, Cell, beginCell } from "@ton/core";
 import { toNano } from "@ton/core";
 import { AerenaCollection } from "../wrappers/AerenaCollection";
+import { AerenaNftItem } from "../wrappers/AerenaNftItem";
 import { compile, NetworkProvider } from "@ton/blueprint";
 
 export async function run(provider: NetworkProvider) {
@@ -17,15 +18,24 @@ export async function run(provider: NetworkProvider) {
         royalty: royalty,
     };
     const aerenaCollection = provider.open(
-        AerenaCollection.createFromConfig(
-            params,
-            await compile("AerenaCollection")
-        )
+        AerenaCollection.createFromConfig(params, compiled)
     );
 
     await aerenaCollection.sendDeploy(provider.sender(), toNano("0.05"));
 
     await provider.waitForDeploy(aerenaCollection.address);
 
+    await aerenaCollection.sendMint(provider.sender(), {
+        queryId: 0,
+        nftAmount: toNano("0.05"),
+        nftId: 0,
+        owner: Address.parse(
+            "UQDC76OniBYJPpP_1uIuq1NUcQBmQUbnLIxaiaV2YNBb6OuC"
+        ),
+        contentUri: "",
+        gas: toNano("0.01"),
+    });
+
+    await provider.waitForDeploy(aerenaCollection.address);
     // run methods on `aerenaCollection`
 }
