18615454dce410d5dc67d339fa35172869f735dd
Ramon Arca <ramonteodoroarca@gmail.com>
2025-02-24 14:10:08 +0800

    Change statText/statValue to just text/value

--- src/components/StatsDisplay/index.tsx
+++ src/components/StatsDisplay/index.tsx
@@ -6,16 +6,16 @@ import { appearAnimation } from "../../helpers/animation";
 import StatsBackground from "../../assets/background/stats.svg";
 
 interface Props {
-    statText: string;
-    statValue: number;
+    text: string;
+    value: number;
 }
-export const StatsDisplay = ({ statText, statValue }: Props) => {
+export const StatsDisplay = ({ text, value }: Props) => {
     const mv = useMotionValue(0);
 
     const stat = useTransform(() => mv.get().toFixed(2));
 
     useEffect(() => {
-        const control = animate(mv, statValue, {
+        const control = animate(mv, value, {
             duration: 2,
         });
 
@@ -30,7 +30,7 @@ export const StatsDisplay = ({ statText, statValue }: Props) => {
                 <img className="h-full w-full" src={StatsBackground} />
                 <div className="absolute flex h-full w-full flex-col items-center justify-center">
                     <p className="mt-[0.8vw] bg-gradient-to-b from-golddark via-goldlight to-golddark bg-clip-text font-russoone text-[2.5vw] font-normal text-transparent">
-                        {statText}
+                        {text}
                     </p>
                     <motion.pre className="-mt-[1vw] font-russoone text-[4.5vw] font-normal text-white">
                         {stat}
