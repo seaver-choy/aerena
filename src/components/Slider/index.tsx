8687b4794f35ae204883527ff20b0339cec38677
Ramon Arca <ramonteodoroarca@gmail.com>
2025-02-25 23:31:27 +0800

    Change BASIC SKIN to DEFAULT CARD

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -55,7 +55,7 @@ export const Slider = ({ athletes, onCardIndexChange }: Props) => {
                 opacity: { wave: getBaseTeamColor().wave },
                 role: athletes[0].position[0],
                 league: athletes[0].league,
-                title: "BASIC SKIN",
+                title: "DEFAULT CARD",
                 type: "default",
             };
             const skinCards = athletes.map((athlete) => {
