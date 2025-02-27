53edf76f49f39aab137c0769173f27498fca9794
bossanarchy <davgam007@gmail.com>
2025-02-27 19:56:11 +0800

    confirm modal updated

--- src/pages/ExchangeScreen/modals/ConfirmModal/index.tsx
+++ src/pages/ExchangeScreen/modals/ConfirmModal/index.tsx
@@ -1,9 +1,6 @@
 import { useEffect } from "react";
 import { motion } from "motion/react";
-import {
-    appearTextAnimation,
-    appearModalAnimation,
-} from "../../../../helpers/animation";
+import { appearTextAnimation } from "../../../../helpers/animation";
 
 import SmallModal from "../../../../assets/modal/small.svg";
 import GoldButton from "../../../../assets/button/gold.svg";
@@ -20,7 +17,13 @@ interface ConfirmModalProps {
     onConfirm: () => void;
 }
 
-export const ConfirmModal = ({ packInfo, costType, boosterQuantity, onCancel, onConfirm }: ConfirmModalProps) => {
+export const ConfirmModal = ({
+    packInfo,
+    costType,
+    boosterQuantity,
+    onCancel,
+    onConfirm,
+}: ConfirmModalProps) => {
     useEffect(() => {
         document.body.style.overflow = "hidden";
 
@@ -33,25 +36,30 @@ export const ConfirmModal = ({ packInfo, costType, boosterQuantity, onCancel, on
         <div className="fixed inset-0 z-40">
             <div className="relative flex h-full w-full items-center justify-center">
                 <div className="h-full w-full bg-graydark opacity-95" />
-                <motion.div
-                    className="absolute z-40 h-[82.5vw] w-[80vw]"
-                    {...appearModalAnimation}
-                >
+                <div className="absolute z-40 h-[82.5vw] w-[80vw]">
                     <img className="h-full w-full" src={SmallModal} />
-                </motion.div>
+                </div>
                 <div className="absolute z-50 flex h-[66.5vw] w-[66vw] flex-col justify-center">
                     <div className="mb-[4vw] flex h-[55vw] flex-col items-center justify-center px-[4vw]">
                         <motion.div {...appearTextAnimation}>
                             <p className="text-center font-montserrat text-[3.5vw] text-graydark">
-                                You are about to purchase {boosterQuantity} Basic Packs for
+                                You are about to purchase {boosterQuantity}{" "}
+                                Basic Packs for
                             </p>
                             <div className="my-[1.5vw] flex justify-center gap-[2vw]">
                                 <img
                                     className="mt-[2.5vw] h-[7.2vw]"
-                                    src={costType === 'star' ? StarsIcon : BattlePointsIcon}
+                                    src={
+                                        costType === "star"
+                                            ? StarsIcon
+                                            : BattlePointsIcon
+                                    }
                                 />
                                 <p className="bg-gradient-to-b from-golddark via-goldlight to-golddark bg-clip-text font-russoone text-[9vw] text-transparent">
-                                    {(costType === 'star' ? packInfo.starCost * boosterQuantity : packInfo.bpCost * boosterQuantity).toLocaleString()}
+                                    {(costType === "star"
+                                        ? packInfo.starCost * boosterQuantity
+                                        : packInfo.bpCost * boosterQuantity
+                                    ).toLocaleString()}
                                 </p>
                             </div>
                             <p className="text-center font-montserrat text-[3.5vw] text-graydark">
@@ -81,7 +89,7 @@ export const ConfirmModal = ({ packInfo, costType, boosterQuantity, onCancel, on
                                 <img className="h-full" src={GoldButton} />
                                 <div className="absolute flex h-full w-full items-center justify-center gap-[1vw]">
                                     <p className="mt-[0.2vw] font-russoone text-[2.8vw] font-normal text-white">
-                                        Purchase
+                                        Confirm
                                     </p>
                                 </div>
                             </motion.button>
