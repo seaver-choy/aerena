c438576d647b2057abd63f1983f57e878af0fd61
Ramon Arca <ramonteodoroarca@gmail.com>
2025-03-11 09:24:44 +0800

    Enable Full Details button

--- src/pages/CollectionScreen/components/StatsSection/index.tsx
+++ src/pages/CollectionScreen/components/StatsSection/index.tsx
@@ -186,7 +186,6 @@ export const StatsSection = ({
                                                         )
                                                     }
                                                     {...appearTextAnimation}
-                                                    disabled
                                                 >
                                                     <img
                                                         className="h-full"
