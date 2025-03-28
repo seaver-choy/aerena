7a3f4eb9c8e2b4f8c0fb69e73e29bf61c0f51626
MaxChronicles <johanssontan12@gmail.com>
2025-03-28 17:01:29 +0800

    upping timeout seconds for some lambda

--- amplify/functions/telegramstars/resource.ts
+++ amplify/functions/telegramstars/resource.ts
@@ -3,6 +3,7 @@ import { defineFunction, secret } from "@aws-amplify/backend";
 export const telegramstarsFunction = defineFunction({
     name: "telegramstars",
     entry: "./handler.ts",
+    timeoutSeconds: 60,
     environment: {
         MONGODB_URI: secret("MONGODB_URI"),
         BOT_TOKEN: secret("BOT_TOKEN"),
