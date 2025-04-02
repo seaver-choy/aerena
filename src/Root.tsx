54b90ff0a75bd41f9ff147b5508e7a64ce1fe849
Seaver Choy <seaver@edamama.ph>
2025-04-02 12:53:33 +0800

    Initialize Telegram Analytics in Prod

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,10 +1,11 @@
-import { type FC, useEffect, useMemo } from "react";
-import { BrowserRouter } from "react-router-dom";
+import TelegramAnalytics from '@telegram-apps/analytics';
 import { SDKProvider, useLaunchParams } from "@telegram-apps/sdk-react";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
-import { UserProvider } from "./hooks/useUser.tsx";
-import { ErrorBoundary } from "./components/ErrorBoundary/index.tsx";
+import { type FC, useEffect, useMemo } from "react";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import { ErrorBoundary } from "./components/ErrorBoundary/index.tsx";
+import { UserProvider } from "./hooks/useUser.tsx";
 
 const ErrorBoundaryError: FC<{ error: unknown }> = ({ error }) => (
     <div>
@@ -20,6 +21,14 @@ const ErrorBoundaryError: FC<{ error: unknown }> = ({ error }) => (
         </blockquote>
     </div>
 );
+
+if(process.env.ENVIRONMENT == "prod") {
+    TelegramAnalytics.init({
+        token: 'eyJhcHBfbmFtZSI6ImFlcmVuYV9ib3QiLCJhcHBfdXJsIjoiaHR0cHM6Ly90Lm1lL2FlcmVuYV9ib3QiLCJhcHBfZG9tYWluIjoiaHR0cHM6Ly9tYWluLmQxcjJpcHk0b2NuMWl6LmFtcGxpZnlhcHAuY29tLyJ9!Kk+TUtACO7B+fC96vRz26KiH8fuKg7cvBdrC8OZW2is=',
+        appName: 'Aerena',
+    });
+}
+
 
 const Inner: FC = () => {
     const debug = useLaunchParams().startParam === "debug";
